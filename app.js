70a5742e6ab3f8cc8f9281039b75acd95fdb94d6
SudreErick <sudrerick@gmail.com>
2025-02-26 02:09:40 -0300

    Resolvendo conflito e alterando limites

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 0 e 20');
+    exibirTextoNaTela('p', 'Escolha um número entre 0 e 100');
 }
 
 exibirMensagemInicial();
